1539c3bb46b0f856bb2062a10f77adf0a3d2062d
Cícero Lopes <lopes138109@gmail.com>
2024-05-08 10:19:09 -0300

    Seno, Cosseno e Tangente
    Um aplicação básica de uma programação para calcular sen, cos e tang de determinados ângulos.
    Exercício básico que trás a importação da biblioteca "import math" e o uso dos camandos "radians " da biblioteca mencionada.

---
+++ Sen, Cos e Tg.py
@@ -0,0 +1,21 @@
+#Calculo dos ângulos SEN,COS E TANG#
+"""import math
+ângulo = float(input("Digite o valor do ângulo \nX= "))
+seno = math.sin(math.radians(ângulo))
+print('O ângulo de {} tem o SENO de {:.2f}'.format(ângulo, seno))
+cosseno = math.cos(math.radians(ângulo))
+print('O ângulo de {} tem o COSSENO de {:.2f}'.format(ângulo, cosseno))
+tangente = math.tan(math.radians(ângulo))
+print('O ângulo de {} tem o valor a TANGENTE de {:.0f}'.format(ângulo, tangente))"""
+
+#Usando com  o calculo com o método from"
+"""from math import radians, sin, cos,tan
+ângulo = float(input('Digite o valor do ângulo \nX= '))
+seno = sin(radians(ângulo))
+print('O ângulo de {} tem o SENO de {:.2f}'.format(ângulo, seno))
+cosseno = cos(radians(ângulo))
+print('O âgulo de {} tem o COSSENO de {:.2f}'.format(ângulo, cosseno))
+tangente = tan(radians(ângulo))
+print('O âgulo de {} tem a TANGENTE DE {:.0f}'.format(ângulo, tangente))"""
+
+
